52f7b8a868d68a1a6c39289de4c443627710f4a5
garde1540 <67937699+garde1540@users.noreply.github.com>
2020-07-07 20:28:37 +0900

    Add files via upload

---
+++ 봇.py
@@ -0,0 +1,23 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("날누르고 메시지 #할말")
+    await client.change_presence(status=discord.Status.online, activity=game)
+      
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("#"):
+        msg = message.content[1:]
+        await client .get_channel(int(730010475831623682)).send(msg)
+
+
+        
+client.run("NzI5OTk2MzcwNTQ2NTI0MjUx.XwRGjA.Ix6a5Cf04-QzmP1T8rrXx_o9VLc")
